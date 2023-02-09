f0111511904563ef60324b3443f3803ac3c250ba
MURCH <47627266+MURChen@users.noreply.github.com>
2023-02-09 18:01:49 +0800

    Update 1.clone.sh

--- preset-immortalwrt/1.clone.sh
+++ preset-immortalwrt/1.clone.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # 下载源码
-git clone --single-branch -b openwrt-21.02 https://github.com/padavanonly/immortalwrt.git
+git clone --single-branch -b openwrt-master https://github.com/padavanonly/immortalwrt.git
 mv ./immortalwrt/* ./
 
 # 下载插件
