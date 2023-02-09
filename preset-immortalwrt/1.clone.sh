021b6212c42ccd5a9a3fe5d222cac37a20c47c3d
MURCH <47627266+MURChen@users.noreply.github.com>
2023-02-09 17:47:47 +0800

    Update 1.clone.sh

--- preset-immortalwrt/1.clone.sh
+++ preset-immortalwrt/1.clone.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # 下载源码
-git clone --single-branch -b openwrt-21.02 https://github.com/immortalwrt/immortalwrt.git
+git clone --single-branch -b openwrt-21.02 https://github.com/padavanonly/immortalwrt.git
 mv ./immortalwrt/* ./
 
 # 下载插件
@@ -10,5 +10,5 @@ echo "src-link supply $PWD/$SUPPLY_DIR" >> feeds.conf.default
 mkdir $SUPPLY_DIR && cd $SUPPLY_DIR
 git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git
 git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config.git
-git clone --depth 1 -b packages https://github.com/xiaorouji/openwrt-passwall.git pw-dependencies
-svn export https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall
+# git clone --depth 1 -b packages https://github.com/xiaorouji/openwrt-passwall.git pw-dependencies
+# svn export https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall
